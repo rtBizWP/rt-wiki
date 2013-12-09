ffd8d789e93c36771125e665728adab15e398255
prannoy <prannoy@prannoy-HP-Pavilion-dv6-Notebook-PC>
2013-12-09 15:56:20 +0530

    Custom template for plugin. #2

--- lib/wiki-single-custom-template.php
+++ lib/wiki-single-custom-template.php
@@ -1,17 +1,19 @@
 <?php
+add_filter( 'template_include', 'rc_tc_template_chooser');
 function rc_tc_template_chooser($template) {
 
     // Post ID
     $post_id = get_the_ID();
 
     // For all other CPT
+
     if (get_post_type($post_id) != 'wiki') {
         return $template;
     }
 
     // Else use custom template
     if (is_single()) {
-        return rc_tc_get_template_hierarchy('single-wiki');
+        return rc_tc_get_template_hierarchy('single');
     }
 }
 
@@ -20,19 +22,31 @@ function rc_tc_template_chooser($template) {
  *
  * @since 1.0
  */
-function rc_tc_get_template_hierarchy($template) {
 
+
+function rc_tc_get_template_hierarchy( $template ) {
+ 
     // Get the template slug
-    $template_slug = rtrim($template, '.php');
+    $template_slug = rtrim( $template, '.php' );
     $template = $template_slug . '.php';
-    
-    $file = RC_TC_BASE_DIR . '/templates/' . $template;
-    return apply_filters('rc_repl_template_' . $template, $file);
+ 
+    // Check if a custom template exists in the theme folder, if not, load the plugin template file
+    if ( $theme_file = locate_template( array($template ) ) ) {
+        $file = $theme_file;
+    }
+    else {
+        $file = RC_TC_BASE_DIR . '/templates/' . $template;
+    }
+ 
+    return apply_filters( 'rc_repl_template_' . $template, $file );
 }
-
-add_filter('template_include', 'rc_tc_template_chooser');
-
-
-
+ 
+/*
+|--------------------------------------------------------------------------
+| FILTERS
+|--------------------------------------------------------------------------
+*/
+ 
+add_filter( 'template_include', 'rc_tc_template_chooser' );
 
 
