f9e287c72ebc912f6e8baebeca0fcc9418a43cc1
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2014-02-18 18:41:22 +0530

    Post Filtering in Feed, Search and Sitemap done

--- lib/wiki-post-filtering.php
+++ lib/wiki-post-filtering.php
@@ -8,76 +8,83 @@
  */
 //require (ABSPATH . WPINC . '/feed.php');
 
-/*
+/**
  * Single Post Content Permission for Wiki CPT
  */
+/* function single_post_filtering() {
+  global $post;
 
-function single_post_filtering() {
-    global $post;
+  $noflag = 0;
+  $noGroup = 0;
+  $readOnly = 0;
 
-    $noflag = 0;
-    $noGroup = 0;
-    $readOnly = 0;
+  $user = get_current_user_id();
+  $terms = get_terms('user-group', array('hide_empty' => true));
+  $access_rights = get_post_meta($post->ID, 'access_rights', true);
 
-    $user = get_current_user_id();
-    $terms = get_terms('user-group', array('hide_empty' => true));
-    $access_rights = get_post_meta($post->ID, 'access_rights', true);
+  if (!is_user_logged_in()) {
 
-    if (!is_user_logged_in()) {
+  if ($access_rights['public']['r'] == 1) {
+  return $post->post_content;
+  }if ($access_rights['public']['na'] == 1) {
+  wp_die(__('Please <a href=' . wp_login_url($post->guid) . '>Login</a> To View the post Content'));
+  }
+  } else {
+  if ($post->post_author == $user) {
 
-        if ($access_rights['public']['r'] == 1) {
-            return $post->post_content;
-        }if ($access_rights['public']['na'] == 1) {
-            wp_die(__('Please <a href=' . wp_login_url($post->guid) . '>Login</a> To View the post Content'));
-        }
-    } else {
-        if ($post->post_author == $user) {
+  return $post->post_content;
+  } else {
+  foreach ($terms as $term) {
 
-            return $post->post_content;
-        } else {
-            foreach ($terms as $term) {
+  $ans = get_term_if_exists($term->slug, $user);
 
-                $ans = get_term_if_exists($term->slug, $user);
+  if ($ans == $term->slug) {
 
-                if ($ans == $term->slug) {
+  if ($access_rights[$ans]['w'] == 1) {
+  return $post->post_content;
+  } else if ($access_rights[$ans]['r'] == 1) {
+  $readOnly = 1;
+  } else if ($access_rights[$ans]['na'] == 1) {
+  $noflag = 1;
+  }
+  } else if ($ans == '' || $ans == null) {
+  $noGroup = 1;
+  }
+  }
+  if ($readOnly == 1) {
+  show_admin_bar(false);
+  return $post->post_content;
+  } else if ($noflag == 1) {
 
-                    if ($access_rights[$ans]['w'] == 1) {
-                        return $post->post_content;
-                    } else if ($access_rights[$ans]['r'] == 1) {
-                        $readOnly = 1;
-                    } else if ($access_rights[$ans]['na'] == 1) {
-                        $noflag = 1;
-                    }
-                } else if ($ans == '' || $ans == null) {
-                    $noGroup = 1;
-                }
-            }
-            if ($readOnly == 1) {
-                show_admin_bar(false);
-                return $post->post_content;
-            } else if ($noflag == 1) {
+  wp_redirect(home_url());
+  wp_die(__('You Do not have permission to access this Content....<a href=' . admin_url() . 'edit.php?post_type=wiki >Back to admin panel</a>'));
+  } else if ($noGroup == 1) {
+  wp_redirect(home_url());
+  wp_die(__('No Permissions found to access this Content...<a href=' . admin_url() . 'edit.php?post_type=wiki >Back to admin panel</a>'));
+  }
 
-                wp_redirect(home_url());
-                wp_die(__('You Do not have permission to access this Content....<a href=' . admin_url() . 'edit.php?post_type=wiki >Back to admin panel</a>'));
-            } else if ($noGroup == 1) {
-                wp_redirect(home_url());
-                wp_die(__('No Permissions found to access this Content...<a href=' . admin_url() . 'edit.php?post_type=wiki >Back to admin panel</a>'));
-            }
+  //        if ($access_rights['all']['w'] == 1) {
+  //            return $post->post_content;
+  //        } else if ($access_rights['all']['r'] == 1) {
+  //            show_admin_bar(false);
+  //            return $post->post_content;
+  //        } else if ($access_rights['all']['na'] == 1) {
+  //
+  //            wp_redirect(home_url());
+  //            wp_die(__('You Do not have permission to access this Content'));
+  //        }
+  }
+  }
+  } */
 
-//        if ($access_rights['all']['w'] == 1) {
-//            return $post->post_content;
-//        } else if ($access_rights['all']['r'] == 1) {
-//            show_admin_bar(false);
-//            return $post->post_content;
-//        } else if ($access_rights['all']['na'] == 1) {
-//            
-//            wp_redirect(home_url());
-//            wp_die(__('You Do not have permission to access this Content'));
-//        }
-        }
-    }
-}
-
+/**
+ * check if term[group] exist or not for perticular user
+ * 
+ * @global type $wpdb
+ * @param type $term : term name 
+ * @param type $userid
+ * @return int
+ */
 function get_term_if_exists($term, $userid) {
 
     global $wpdb;
@@ -86,10 +93,13 @@ function get_term_if_exists($term, $userid) {
     return $page_id;
 }
 
-/*
+/**
  * removes quick edit from wiki post type
+ * 
+ * @global type $post
+ * @param type $actions
+ * @return type
  */
-
 function remove_quick_edit($actions) {
     global $post;
     $supported_posts = rtwiki_get_supported_attribute();
@@ -106,32 +116,38 @@ function remove_quick_edit($actions) {
 
 add_filter('page_row_actions', 'remove_quick_edit', 10);
 
-
-/*
+/**
  * Check permissions at Admin side for edit post 
  */
-
 function postCheck() {
     if (isset($_GET['action']) && $_GET['action'] == 'edit') {
         $page = isset($_GET['post']) ? $_GET['post'] : 0;
         $supported_posts = rtwiki_get_supported_attribute();
-        // $status = get_post_meta($page, '_edit_last');
-        //if ($status[0] == '1') {
         if (in_array(get_post_type($page), $supported_posts)) {
             if (getAdminPanelSidePermission($page) == false) {
                 WP_DIE(__('You Dont have enough access rights to Edit this post'));
             }
-            // }
         }
     }
 }
 
 add_action('admin_init', 'postCheck');
 
+/**
+ * add capabilities for perticular user
+ * 
+ * @global type $post
+ * @param type $capabilities
+ * @param type $cap
+ * @param type $args
+ * @param type $user
+ * @return type
+ */
 function addCapabilities($capabilities, $cap, $args, $user) {
     global $post;
     $access = 'na';
     $supported_posts = rtwiki_get_supported_attribute();
+
     if (is_object($post) && in_array($post->post_type, $supported_posts)) {
         $access = getAdminPanelSidePermission($post->ID);
         if ($access == false) {
@@ -174,10 +190,13 @@ function addCapabilities($capabilities, $cap, $args, $user) {
 
 add_filter('user_has_cap', 'addCapabilities', 10, 4);
 
-/*
+/**
  * Checks the permission of the Logged in User for editing post
+ * 
+ * @global type $current_user
+ * @param type $pageID
+ * @return string|boolean
  */
-
 function getAdminPanelSidePermission($pageID) {
 
     $noflag = 0;
@@ -203,25 +222,33 @@ function getAdminPanelSidePermission($pageID) {
             if (empty($access_rights)) {
                 return true;
             }
-            foreach ($terms as $term) {
+            if (isset($access_rights['public']) && 1 == $access_rights['public']) {
+                return 'r';
+            } else if (isset($access_rights['all'])) {
 
-                $ans = get_term_if_exists($term->slug, $user);
-
-                if ($ans == $term->slug && isset($access_rights[$ans])) {
-
-                    if (isset($access_rights[$ans]['w']) && ( $access_rights[$ans]['w'] == 1 )) {
-                        return 'w';
-                    } else if (isset($access_rights[$ans]['r']) && ( $access_rights[$ans]['r'] == 1 )) {
-                        return 'r';
-                    } else if (isset($access_rights[$ans]['na']) && ( $access_rights[$ans]['na'] == 1 )) {
-                        $noflag = 1;
+                if (isset($access_rights['all']['w']) && ( $access_rights['all']['w'] == 1 )) {
+                    return 'w';
+                } else if (isset($access_rights['all']['r']) && ( $access_rights['all']['r'] == 1 )) {
+                    return 'r';
+                } else if (isset($access_rights['all']['na']) && ( $access_rights['all']['na'] == 1 )) {
+                    $noflag = 1;
+                }
+            } else {
+                foreach ($terms as $term) {
+                    $ans = get_term_if_exists($term->slug, $user);
+                    if ($ans == $term->slug && isset($access_rights[$term->name])) {
+                        if (isset($access_rights[$term->name]['w']) && ( $access_rights[$term->name]['w'] == 1 )) {
+                            return 'w';
+                        } else if (isset($access_rights[$term->name]['r']) && ( $access_rights[$term->name]['r'] == 1 )) {
+                            return 'r';
+                        } else if (isset($access_rights[$term->name]['na']) && ( $access_rights[$term->name]['na'] == 1 )) {
+                            $noflag = 1;
+                        }
+                    } else if ($ans == '' || $ans == null) {
+                        $noPublic = 1;
                     }
-                } else if ($ans == '' || $ans == null) {
-                    $noPublic = 1;
                 }
             }
-
-
             if ($noflag == 1 || $noPublic == 1) {
                 return false;
             }
@@ -229,22 +256,20 @@ function getAdminPanelSidePermission($pageID) {
     }
 }
 
-/*
+/**
  * Checks the permission of the user for the post(Frontend)
+ * 
+ * @param type $pageID
+ * @param type $user
+ * @return boolean
  */
-
-function getPermission($pageID) {
-
+function getPermission($pageID, $user) {
     $noflag = 0;
     $noPublic = 0;
-    $user = get_current_user_id();
     $terms = get_terms('user-group', array('hide_empty' => true));
     $access_rights = get_post_meta($pageID, 'access_rights', true);
-
     if (isset($access_rights['public']) && ( 1 == $access_rights['public'] )) {
         return true;
-    } else if (isset($access_rights['public']) && !is_user_logged_in() && ( 0 == $access_rights['public'] )) {
-        return false;
     } else if (is_user_logged_in()) {
         $post_details = get_post($pageID);
 
@@ -276,62 +301,12 @@ function getPermission($pageID) {
         if ($noPublic == 1) {
             return false;
         }
+    } else if (isset($access_rights['public']) && !is_user_logged_in() && ( 0 == $access_rights['public'] )) {
+        return false;
     }
 }
 
 add_filter('bulk_actions-' . 'edit-wiki', '__return_empty_array');
-
-/**
- * Function to unsubscribe/remove userid from post meta 
- * 
- * @param 
- *      int $postid
- *      int $userid
- */
-function unSubscription($postid, $userid) {
-    global $rtWikiSubscribe;
-    if ($rtWikiSubscribe->is_subscribe($postid, $userid)) {
-        $subscriber = array(
-            'attribute_postid' => $postid,
-            'attribute_userid' => $userid
-        );
-        $rtWikiSubscribe->delete_subscriber($subscriber);
-    }
-}
-
-/**
- * Function to unsubscribe/remove userid from post meta 
- * 
- * @param 
- *      int $postid
- *      int $userid
- */
-function unSubscriptionAll($postid) {
-    global $rtWikiSubscribe;
-    $subscriber = array(
-        'attribute_postid' => $postid,
-    );
-    $rtWikiSubscribe->delete_subscriber($subscriber);
-}
-
-/**
- * Function to unsubscribe/remove userid from post meta 
- * 
- * @param 
- *      int $postid
- *      int $userid
- */
-function pageSubscription($postid, $userid, $is_sub_subscribe) {
-    global $rtWikiSubscribe;
-    if (!$rtWikiSubscribe->is_subscribe($postid, $userid)) {
-        $subscriber = array(
-            'attribute_postid' => $postid,
-            'attribute_userid' => $userid,
-            'attribute_sub_subscribe' => $is_sub_subscribe
-        );
-        $rtWikiSubscribe->add_subscriber($subscriber);
-    }
-}
 
 /* Function to disable feeds for wiki CPT */
 remove_action('do_feed_rdf', 'do_feed_rdf', 10, 1);
@@ -345,61 +320,204 @@ add_action('do_feed_rss', 'my_do_feed', 10, 1);
 add_action('do_feed_rss2', 'my_do_feed', 10, 1);
 add_action('do_feed_atom', 'my_do_feed', 10, 1);
 
-// Finally, we do the post type check, and generate feeds conditionally
+/**
+ * Finally, we do the post type & permission on CPT check, and generate feeds conditionally
+ * 
+ * @global type $wp_query
+ * @global type $post
+ */
 function my_do_feed() {
-    global $wp_query;
+    global $wp_query, $post;
     $no_feed = array('wiki');
     if (in_array($wp_query->query_vars['post_type'], $no_feed)) {
         wp_die(__('This is not a valid feed address.', 'textdomain'));
     } else {
+        $supported_posts = rtwiki_get_supported_attribute();
+        $newpostArray = array();
+        if (isset($wp_query->posts)) {
+            foreach ($wp_query->posts as $post) {
+                if (in_array(get_post_type(), $supported_posts)) {
+                    if (getPermission(get_the_ID(), get_current_user_id())) {
+                        $newpostArray[] = $post;
+                    }
+                } else {
+                    $newpostArray[] = $post;
+                }
+            }
+            $wp_query->posts = $newpostArray;
+            $wp_query->post_count = count($newpostArray);
+            $wp_query->found_posts = count($newpostArray);
+        }
+        if (isset($wp_query->comments)) {
+            $newCommentArray = array();
+            foreach ($wp_query->comments as $comment) {
+                if (in_array(get_post_type($comment->comment_post_ID), $supported_posts)) {
+                    if (getPermission($comment->comment_post_ID, get_current_user_id())) {
+                        $newCommentArray[] = $comment;
+                    }
+                } else {
+                    $newpostArray[] = $post;
+                }
+            }
+            $wp_query->comments = $newCommentArray;
+            $wp_query->comment_count = count($newpostArray);
+        }
         do_feed_rss2($wp_query->is_comment_feed);
     }
 }
 
-/* Changing taxnonmy query for wiki CPT */
-add_action('pre_get_posts', 'add_wiki_taxonomy');
-
-function add_wiki_taxonomy($query) {
-    //global $post;
-
-    if (is_admin()) {
-        return false;
-    }
-
-    if (is_archive() && is_tax()) {
-
-        $tax = get_queried_object();
-        $objectArray = get_taxonomy($tax->taxonomy);
-
-        if (array_key_exists('object_type', $objectArray)) {
-            $post_types = $objectArray->object_type;
-
-            if (in_array('wiki', $post_types)) {
-                if ($query->is_tax()) {
-                    $query->set('post_type', 'wiki');
+/**
+ * check permission for CPT and generate result[get_posts] 
+ * 
+ * @param type $wp_query
+ * @return type
+ */
+function rtwiki_search_filter($wp_query) {
+    $supported_posts = rtwiki_get_supported_attribute();
+    if (isset($wp_query->posts) && is_search()) {
+        foreach ($wp_query->posts as $post) {
+            if (in_array($post->post_type, $supported_posts)) {
+                if (getPermission($post->ID, get_current_user_id())) {
+                    $newpostArray[] = $post;
                 }
+            } else {
+                $newpostArray[] = $post;
             }
         }
+        $wp_query->posts = $newpostArray;
+        $wp_query->post_count = count($newpostArray);
+        $wp_query->found_posts = count($newpostArray);
     }
+    return $wp_query;
 }
 
+add_action('loop_start', 'rtwiki_search_filter');
+
+/**
+ * check permission for CPT content.
+ * 
+ * @param type $content
+ * @return type
+ */
 function rtwiki_content_filter($content) {
-    if (getPermission(get_the_ID())) {
-        $post_thumbnail = get_the_post_thumbnail();
-        return $post_thumbnail . $content;
-    } else {
-        return '<p>' . __('Not Enough Rights to View The Content.', 'rtCamp') . '</p>';
+    $supported_posts = rtwiki_get_supported_attribute();
+    if (in_array(get_post_type(), $supported_posts)) {
+        if (getPermission(get_the_ID(), get_current_user_id())) {
+            $post_thumbnail = get_the_post_thumbnail();
+            return $post_thumbnail . $content;
+        } else {
+            return '<p>' . __('Not Enough Rights to View The Content.', 'rtCamp') . '</p>';
+        }
     }
+    return $content;
 }
 
 add_filter('the_content', 'rtwiki_content_filter');
 
+/**
+ * check permission for CPT edit content link.
+ * 
+ * @param type $output
+ * @return string
+ */
 function rtwiki_edit_post_link_filter($output) {
-    if (getPermission(get_the_ID())) {
-        return $output;
-    } else {
-        return '';
+    $supported_posts = rtwiki_get_supported_attribute();
+    if (in_array(get_post_type(), $supported_posts)) {
+        if (getPermission(get_the_ID(), get_current_user_id())) {
+            return $output;
+        } else {
+            return '';
+        }
     }
+    return $output;
 }
 
 add_filter('edit_post_link', 'rtwiki_edit_post_link_filter');
+
+/**
+ * check permission before cpt comment shows.
+ * 
+ * @param type $comments
+ * @param type $post_id
+ * @return type
+ */
+function rtwiki_comment_filter($comments, $post_id) {
+    //@TODO is wiki post type
+    $supported_posts = rtwiki_get_supported_attribute();
+    $post = get_post($post_id);
+    if (in_array($post->post_type, $supported_posts)) {
+        if (getPermission($post_id, get_current_user_id())) {
+            return $comments;
+        }
+        return array();
+    }
+    return $comments;
+}
+
+add_filter('comments_array', 'rtwiki_comment_filter', 10, 2);
+
+/**
+ * check permission before cpt comment form display.
+ * 
+ * @param type $open
+ * @param type $post_id
+ * @return boolean
+ */
+function rtwiki_comment_form_filter($open, $post_id) {
+    //@TODO is wiki post type
+    $supported_posts = rtwiki_get_supported_attribute();
+    $post = get_post($post_id);
+    if (in_array($post->post_type, $supported_posts)) {
+        if (getPermission($post_id, get_current_user_id())) {
+            return $open;
+        }
+        return FALSE;
+    }
+    return $open;
+}
+
+add_filter('comments_open', 'rtwiki_comment_form_filter', 10, 2);
+
+/**
+ * sitemap taxonomies filter [Yoast plugin]
+ * 
+ * @global type $rtWikiAttributesModel
+ * @param type $taxonomies
+ * @return type
+ */
+function rtwiki_sitemap_taxonomies($taxonomies) {
+    global $rtWikiAttributesModel;
+    $attributes = $rtWikiAttributesModel->get_all_attributes();
+    $customAttributes = array();
+    foreach ($attributes as $attribute) {
+        $customAttributes[] = $attribute->attribute_name;
+    }
+    foreach ($taxonomies as $key => $val) {
+        if (in_array($key, $customAttributes)) {
+            unset($taxonomies[$key]);
+        }
+    }
+    return $taxonomies;
+}
+
+add_filter('wpseo_sitemaps_supported_taxonomies', rtwiki_sitemap_taxonomies);
+
+/**
+ * sitemap posttype filter [Yoast plugin]
+ * 
+ * @global type $rtWikiAttributesModel
+ * @param type $posttypes
+ * @return type
+ */
+function rtwiki_sitemap_posttypes($posttypes) {
+    global $rtWikiAttributesModel;
+    $attributes = rtwiki_get_supported_attribute();
+    foreach ($posttypes as $key => $val) {
+        if (in_array($key, $attributes)) {
+            unset($posttypes[$key]);
+        }
+    }
+    return $posttypes;
+}
+
+add_filter('wpseo_sitemaps_supported_post_types', rtwiki_sitemap_posttypes);
