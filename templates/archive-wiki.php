8a6e884c2401758677dd21567de5d6559dc58af1
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2014-02-25 16:31:08 +0530

    wiki archive change

--- templates/archive-wiki.php
+++ templates/archive-wiki.php
@@ -13,19 +13,8 @@ $content_class = apply_filters( 'rtwiki_content_class', 'large-8 columns rtp-sin
 	<section id="content" class="rtp-content-section <?php echo $content_class ?> " role="main">
 
 		<header class="page-header">
-			<h1 class="page-title"><?php
-if ( is_day() ) :
-	printf( __( 'Daily Archives: %s', 'rtCamp' ), get_the_date() );
-elseif ( is_month() ) :
-	printf( __( 'Monthly Archives: %s', 'rtCamp' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'rtCamp' ) ) );
-elseif ( is_year() ) :
-	printf( __( 'Yearly Archives: %s', 'rtCamp' ), get_the_date( _x( 'Y', 'yearly archives date format', 'rtCamp' ) ) );
-else :
-	_e( 'Archives', 'rtCamp' );
-endif;
-	?>
-			</h1>
 		</header>
+
 		<article id="docslist" class='clearfix rtp-post-box'>
 			<div class="post-content">
 
@@ -39,7 +28,7 @@ endif;
 					'include' => '',
 					'link_after' => '',
 					'link_before' => '',
-					'post_type' => 'docs',
+					'post_type' => get_post_type(),
 					'post_status' => 'publish',
 					'show_date' => '',
 					'sort_column' => 'menu_order, post_title',
