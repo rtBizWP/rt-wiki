132e0f47c7ed8f50cffa28126b7068b92c8a723f
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2014-02-25 16:00:04 +0530

    wiki archive change

--- templates/archive-wiki.php
+++ templates/archive-wiki.php
@@ -4,58 +4,58 @@
  *
  * @package rtPanel
  *
- * @since rtPanelChild 2.0
+ * @since   rtPanelChild 2.0
  */
 get_header();
 
-$content_class = apply_filters( 'rtwiki_content_class', 'large-8 small-12 columns' );
+$content_class = apply_filters( 'rtwiki_content_class', 'large-8 columns rtp-singular' );
 ?>
+	<section id="content" class="rtp-content-section <?php echo $content_class ?> " role="main">
 
-<div id="primary" class="content-area <?php echo $content_class; ?>">
-    <div id="content" class="site-content" role="main">
-
-        <header class="page-header">
-            <h1 class="page-title"><?php
+		<header class="page-header">
+			<h1 class="page-title"><?php
 if ( is_day() ) :
-                printf(__('Daily Archives: %s', 'rtCamp'), get_the_date());
+	printf( __( 'Daily Archives: %s', 'rtCamp' ), get_the_date() );
 elseif ( is_month() ) :
-                printf(__('Monthly Archives: %s', 'rtCamp'), get_the_date(_x('F Y', 'monthly archives date format', 'rtCamp')));
-elseif (is_year()) :
-                printf(__('Yearly Archives: %s', 'rtCamp'), get_the_date(_x('Y', 'yearly archives date format', 'rtCamp')));
-            else :
-                _e('Archives', 'rtCamp');
-            endif;
-            ?></h1>
-        </header>
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                    <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> >
-                        <header class="entry-header">
-                            <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf( esc_attr__( 'Permanent Link to %s', 'rtCamp' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h2>
-                        </header>
-                        <div class="entry-content">
-                            <?php the_excerpt(); ?>
-                        </div>
-                    </article>
-            <?php
-                endwhile;
-            ?>
-            <div class="navigation">
-                <div class="alignleft"><?php previous_posts_link('&laquo; Previous Page') ?></div>
-                <div class="alignright"><?php next_posts_link('Next Page &raquo;', '') ?></div>
-            </div>
-           
+	printf( __( 'Monthly Archives: %s', 'rtCamp' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'rtCamp' ) ) );
+elseif ( is_year() ) :
+	printf( __( 'Yearly Archives: %s', 'rtCamp' ), get_the_date( _x( 'Y', 'yearly archives date format', 'rtCamp' ) ) );
+else :
+	_e( 'Archives', 'rtCamp' );
+endif;
+	?>
+			</h1>
+		</header>
+		<article id="docslist" class='clearfix rtp-post-box'>
+			<div class="post-content">
 
-        <?php else : ?>
-            <?php get_template_part('content', 'none'); ?>
-        <?php endif; ?>
+				<?php
+				$args = array(
+					'authors' => '',
+					'child_of' => 0,
+					'depth' => 0,
+					'echo' => 0,
+					'exclude' => '',
+					'include' => '',
+					'link_after' => '',
+					'link_before' => '',
+					'post_type' => 'docs',
+					'post_status' => 'publish',
+					'show_date' => '',
+					'sort_column' => 'menu_order, post_title',
+					'title_li' => __( '<p>Below is list of Docs that can help you with different aspects:</p>' ),
+					'walker' => '', );
 
+$wikis = wp_list_pages( $args );
 
-
-
-
-    </div><!-- #content -->
-</div><!-- #primary -->
-
-<?php get_sidebar(); 
- get_footer();
+if ( isset( $wikis ) ){
+	echo $wikis;
+} else {
+	get_template_part( 'content', 'none' );
+}
+	?>
+			</div>
+		</article>
+	</section>
+<?php get_sidebar();
+get_footer();
