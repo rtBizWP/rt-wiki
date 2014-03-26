41d9c465483d53a62e4c567b29158c2c5a7ab216
dipesh kakadiya <dipesh.kakadiya@rtcamp.com>
2014-03-26 18:48:24 +0530

    JScript for permission meta-box updated.

--- app/assets/js/rtwiki-admin-script.js
+++ app/assets/js/rtwiki-admin-script.js
@@ -4,27 +4,33 @@
  */
 
 
-function uncheckAll(arg) {
-    if (arg == 'na' || arg == 'r' || arg == 'w') {
-        jQuery('.rtwiki_all_na').prop('checked', false);
-        jQuery('.rtwiki_all_w').prop('checked', false);
-    }
-    if (arg == 'na' || arg == 'r') {
-        jQuery('.rtwiki_all_r').prop('checked', false);
-    }
+function uncheckAll(element,arg) {
+	if (element.checked) {
+	    if (arg == 'na' || arg == 'r' || arg == 'w') {
+	        jQuery('.rtwiki_all_na').prop('checked', false);
+	        jQuery('.rtwiki_all_w').prop('checked', false);
+		    jQuery('.rtwiki_all_r').prop('checked', false);
+	    }
+	}
+	jQuery(".rtwiki_pub").prop("checked", false);
+	jQuery(".rtwiki_na").prop("disabled", false);
 }
 
-function uncheckAllGroup(arg) {
-    if (arg == 'na') {
-        jQuery('.case_na').prop('checked', true);
-        jQuery('.case_r').prop('checked', false);
-        jQuery('.case_w').prop('checked', false);
-    } else if (arg == 'r') {
-        jQuery('.case_na').prop('checked', false);
-        jQuery('.case_r').prop('checked', true);
-    } else if (arg == 'w') {
-        jQuery('.case_na').prop('checked', false);
-        jQuery('.case_w').prop('checked', true);
-    }
+function uncheckAllGroup(element,arg) {
+	if (element.checked) {
+	    if (arg == 'na') {
+	        jQuery('.rtwiki_na').prop('checked', true);
+	        jQuery('.rtwiki_r').prop('checked', false);
+	        jQuery('.rtwiki_w').prop('checked', false);
+	    } else if (arg == 'r') {
+	        jQuery('.rtwiki_na').prop('checked', false);
+	        jQuery('.rtwiki_r').prop('checked', true);
+	    } else if (arg == 'w') {
+	        jQuery('.rtwiki_na').prop('checked', false);
+	        jQuery('.rtwiki_w').prop('checked', true);
+	    }
+	}
+	jQuery(".rtwiki_pub").prop("checked", false);
+	jQuery(".rtwiki_na").prop("disabled", false);
 }
 
