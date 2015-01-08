818fa9682fd63a3df6244a0e41f003cd93bf4747
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2015-01-08 16:40:23 +0530

    lib remove from wiki

--- app/lib/tests/tests/test-rt-plugin-update.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Test_RT_Plugin_Update extends RT_WP_TestCase {
-	public $rtpluginupdate;
-	public function test_constructor() {
-		//https://rtcamp.com/wp-content/uploads/wp-helper-plugin.json
-
-		$this->rtpluginupdate;
-	}
-}
